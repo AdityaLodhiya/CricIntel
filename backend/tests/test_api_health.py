c42ed0cc2a0aafd47499ac0abb55df9fd18306fd
Aditya <adityalodhiya111@gmail.com>
2026-07-29 10:37:22 +0530

    Some fix

--- backend/tests/test_api_health.py
+++
@@ -1,8 +0,0 @@
-"""Backend integration tests placeholder."""
-
-from django.test import TestCase
-
-
-class APIHealthTests(TestCase):
-    def test_placeholder(self):
-        self.assertTrue(True)
--- frontend/src/pages/AboutPage.jsx
+++
@@ -1,7 +0,0 @@
-import ComingSoon from '../components/ComingSoon';
-
-function AboutPage() {
-  return <ComingSoon pageName="About" />;
-}
-
-export default AboutPage;
